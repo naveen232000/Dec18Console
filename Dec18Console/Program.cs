c25f9679a19b3b773632c00ed7e3f6185eb6d811
mygit@23 <naveenmadhesan23@gmail.com>
2023-12-18 16:41:49 +0530

    Add project files.

---
+++ Dec18Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec18Console
+{
+    internal class Program
+    {
+        public static void JaggedarrayFun()
+        {
+            int[][] arr = new int[3][];// Declare the array  
+
+            arr[0] = new int[] { 11, 21, 56};// Initialize the array          
+            arr[1] = new int[] { 42, 61, 41, 59, 63 };
+            arr[2] = new int[] { 1, 2, 3 };
+
+            // array elements  
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            JaggedarrayFun();
+            Console.Read();
+        }
+    }
+}
